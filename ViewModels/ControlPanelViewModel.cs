a9bb275e84686c9d3d1f4dda51aa6cdc2efa91e6
kofund <ejjung312@naver.com>
2024-11-08 17:20:07 +0900

    컨트롤패널 작업중

--- ViewModels/ControlPanelViewModel.cs
+++ ViewModels/ControlPanelViewModel.cs
@@ -5,6 +5,7 @@ using Models;
 using Services;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using ViewModels.Messaging;
 
 namespace ViewModels
 {
@@ -38,6 +39,62 @@ namespace ViewModels
                 return _fileOpenCommand ??
                     (_fileOpenCommand = new RelayCommand<object?>(this.FileOpenExecute));
             }
+        }
+
+        private RelayCommand _play_pauseCommand;
+        public RelayCommand PlayPauseCommand
+        {
+            get
+            {
+                return _play_pauseCommand ??
+                    (_play_pauseCommand = new RelayCommand(this.PlayPauseExecute, this.CanPlayPauseExecute));
+            }
+        }
+
+        private bool CanPlayPauseExecute()
+        {
+            if (PlayInfoModel is null) return false;
+
+            if (PlayInfoModel.IsPlaying is false)
+            {
+                return PlayInfoModel.CanPlay;
+            }
+            else
+            {
+                return PlayInfoModel.IsPlaying;
+            }
+        }
+
+        private void PlayPauseExecute()
+        {
+            try
+            {
+                if (PlayInfoModel.CanPlay is true &&
+                    PlayInfoModel.IsPlaying is false)
+                {
+                    this.Play();
+                }
+                else if (PlayInfoModel.CanPlay is false &&
+                    PlayInfoModel.IsPlaying is true)
+                {
+                    _bassService.Pause();
+                    PlayPauseCommand.NotifyCanExecuteChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log.Error(ex);
+            }
+        }
+
+        private void Play()
+        {
+            // 최초 OpenFile 이전에는 볼륨 세팅이 되지 않기 때문에 Play시 볼륨 세팅한다.
+            _bassService.SetVolume(VolumePosition);
+            _bassService.Play();
+            PlayPauseCommand.NotifyCanExecuteChanged();
+
+            WeakReferenceMessenger.Default.Send(new SetPlayInfoMessage(PlayInfoModel));
         }
 
         private void FileOpenExecute(object? filePaths)
